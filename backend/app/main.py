a94109e23eefe1ca7e64e2b5cb750dfeee2637b1
deepakwaghela829-ops <deepakwaghela829@gmail.com>
2026-09-24 17:14:06 +0530

    Allow frontend access to API

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
